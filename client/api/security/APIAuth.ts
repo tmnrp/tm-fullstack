914d82ad367fc323a6b978ae1d14b039e16e5fc3
TMNRP <tony.venom007@gmail.com>
2022-03-04 20:31:08 +0530

    build bug fix

--- client/api/security/APIAuth.ts
+++ client/api/security/APIAuth.ts
@@ -1,6 +1,5 @@
 import { AxiosResponse } from "axios";
 import { AxiosRequest } from "..";
-import { CONST_CONFIG } from "../../utils/contants";
 
 //
 export const APIAuth = {
@@ -9,7 +8,7 @@ export const APIAuth = {
     callback: (res: AxiosResponse<any, any>) => void
   ) => {
     const request = AxiosRequest({
-      baseURL: CONST_CONFIG.BASE_URL,
+      baseURL: AxiosRequest.BASE_URL,
     });
     const res = await request.post("/api/auth/login", {
       username: data.credNm,
